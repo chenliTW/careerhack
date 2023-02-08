7e164a733c2a7e6779081a574e8d46c7baf21026
chenlitw <ab0897867564534231@gmail.com>
2023-02-08 14:07:46 +0800

    active-passive !!!

--- ha-network-service-codebase/Storage/check-readiness.sh
+++ ha-network-service-codebase/Storage/check-readiness.sh
@@ -3,8 +3,8 @@
 podhealth() {
         if [[ $MY_NODE_NAME -eq "node1" ]]
           then
-            activepod=$(for i in 0 1; do echo careerhack-inventory-$i;done | grep -v $HOSTNAME)
-            curl -I $activepod.careerhack-inventory.default.svc.cluster.local:8200
+            activepod=$(for i in 0 1; do echo careerhack-storage-$i;done | grep -v $HOSTNAME)
+            curl -I $activepod.careerhack-storage.default.svc.cluster.local:8300
             if [[ $? -ne 0 ]]
               then
                 return 0
