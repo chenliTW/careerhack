8b48a65f6e960f54269fe906a914e8a52b7b4907
chenlitw <ab0897867564534231@gmail.com>
2023-02-08 17:24:49 +0800

    better rollout script

--- rollout.sh
+++ rollout.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 before=$(cat /tmp/kbefore.txt)
-if [ -n "$before" ] && [ $before -ne $(kubectl get node | grep " Ready" | wc -l) ]; then
+
+echo `expr $before`;
+echo `expr $(kubectl get node | grep " Ready" | wc -l)`;
+
+if [ -n "$before" ] && [ `expr $before` -lt `expr $(kubectl get node | grep " Ready" | wc -l)` ]; then
   #kubectl rollout restart deployment careerhack-business
   sleep 20
   kubectl rollout restart statefulset.apps/careerhack-inventory
