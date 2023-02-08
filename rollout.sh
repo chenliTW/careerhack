cde0d1bf75d3d448b0ab723a88809422833ae182
chenlitw <ab0897867564534231@gmail.com>
2023-02-08 19:59:39 +0800

    Revert "better rollout script"
    This reverts commit 8b48a65f6e960f54269fe906a914e8a52b7b4907.

--- rollout.sh
+++ rollout.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 before=$(cat /tmp/kbefore.txt)
-
-echo `expr $before`;
-echo `expr $(kubectl get node | grep " Ready" | wc -l)`;
-
-if [ -n "$before" ] && [ `expr $before` -lt `expr $(kubectl get node | grep " Ready" | wc -l)` ]; then
+if [ -n "$before" ] && [ $before -ne $(kubectl get node | grep " Ready" | wc -l) ]; then
   #kubectl rollout restart deployment careerhack-business
   sleep 20
   kubectl rollout restart statefulset.apps/careerhack-inventory
