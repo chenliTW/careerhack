8640f86b81bc7244e7e0e81356aa10db582eba8f
chenlitw <ab0897867564534231@gmail.com>
2023-02-08 16:12:11 +0800

    better deployment

--- rollout.sh
+++ rollout.sh
@@ -3,7 +3,7 @@ before=$(cat /tmp/kbefore.txt)
 if [ -n "$before" ] && [ $before -ne $(kubectl get node | grep " Ready" | wc -l) ]; then
   #kubectl rollout restart deployment careerhack-business
   sleep 20
-  kubectl rollout restart deployment careerhack-inventory
-  kubectl rollout restart deployment careerhack-storage
+  kubectl rollout restart statefulset.apps/careerhack-inventory
+  kubectl rollout restart statefulset.apps/careerhack-storage
 fi
 echo $(kubectl get node | grep " Ready" | wc -l) > /tmp/kbefore.txt
