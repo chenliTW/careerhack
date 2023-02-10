84b7f5741fc7abbda7aff157531a397830fcd7bd
Shadowcollecter <shadowcollect@gmail.com>
2023-02-10 13:50:53 +0800

    random test's order api fully functioned

--- ha-network-service-codebase/Tester/RandomTester/script.js
+++ ha-network-service-codebase/Tester/RandomTester/script.js
@@ -9,8 +9,8 @@ export const options = {
     scenarios: {
       send: {
         executor: 'shared-iterations',
-        vus: 100, // number of threads
-        iterations: 100000,
+        vus: 1, // number of threads
+        iterations: 1,
         maxDuration: '120s',
       },
     },
@@ -54,9 +54,9 @@ const ORDERApi = () => {
   //const data = ORDERpayload(); // random data
   const payload_1  = JSON.stringify(data);
   //const res = http.post('http://34.80.73.51:80/api/order', stable, {headers});//tsmc stable
-  const res = http.post('http://34.80.73.51:80/api/order', payload_1, {headers});//tsmc random
+  //const res = http.post('http://34.80.73.51:80/api/order', payload_1, {headers});//tsmc random
   //const res = http.post('http://34.80.73.51:80/api/order', payload_1, params);//tsmc with params
-  //const res = http.post('http://34.123.52.100:30100/api/order', stable, {headers});//stable old
+  const res = http.post('http://34.123.52.100:30100/api/order', stable, {headers});//stable old
   //const res = http.post('http://34.123.52.100:30100/api/order', payload_1, {headers});//random old
 
   check(res, {
@@ -96,21 +96,18 @@ const REPORTApi = () => {
 
   const location  = 'location='.concat('l1');
   const date = 'date='.concat('2021-03-01T00:00:00.000Z');
-  const url = 'http://34.80.73.51:8100/api/report?';
+  const url = 'http://34.123.52.100:30100/api/report?';
   const sent = url.concat(location, '&', date);
-  const res = http.get(sent);
-  console.log(res.json().material);
-  console.log(res.json().a);
-  console.log(res.json().b);
-  console.log(res.json().c);
-  console.log(res.json().d);
+  //const res = http.get(sent);
+  const res = http.get('http://34.123.52.100:30100/api/report?location=l1&date=2023-01-01');  
+  console.log(res);
   check(res,{
     'GET status is 200 -REPORT': (r) => res.status === 200,
-    'date & location is correct': (r) => res.json().location === 'l1',
+    //'date & location is correct': (r) => res.json().location === 'l1',
   })
 }
 export default function () {
-  ORDERApi();
+  //ORDERApi();
   //RECORDApi();
-  //REPORTApi();
+  REPORTApi();
 }
