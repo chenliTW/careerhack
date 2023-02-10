c4f1ea11e5f0da997b6a1eda8f29900e76c48a0c
Shadowcollecter <shadowcollect@gmail.com>
2023-02-10 11:56:05 +0800

    update random tester

--- ha-network-service-codebase/Tester/RandomTester/script.js
+++ ha-network-service-codebase/Tester/RandomTester/script.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import { POSTpayload } from './POSTfake.js';
-import { GETKeys, GETpayload } from './GETfake.js';
+import { ORDERpayload } from './ORDERfake.js';
+import { RECORDKeys} from './RECORDfake.js';
 
 
 export const options = {
@@ -9,14 +9,14 @@ export const options = {
       send: {
         executor: 'shared-iterations',
         vus: 1000, // number of threads
-        iterations: 10000,
-        maxDuration: '30s',
+        iterations: 1000,
+        maxDuration: '120s',
       },
     },
 };
 
 
-const POSTApi = () => {
+const ORDERApi = () => {
 
   const stable = JSON.stringify({
     "location": "l1",
@@ -34,33 +34,63 @@ const POSTApi = () => {
     'Content-Type': 'application/json',
   };
 
-  const data = POSTpayload();
+  const data = ORDERpayload();
   const payload_1  = JSON.stringify(data);
-  const res = http.post('http://34.123.52.100:30100/api/order', payload_1, {headers});
-  //const res = http.post('http://34.123.52.100:30100/api/order', stable, {headers});
+  const res = http.post('http://34.80.73.51:80/api/order', payload_1, {headers});//random
+  //const res = http.post('http://34.123.52.100:30100/api/order', stable, {headers});//stable
+  //const res = http.post('http://34.80.73.51:80/api/order', stable, {headers});//old
 
   check(res, {
-    'Post status is 200 -1': (r) => res.status === 200,
+    'Post status is 200 -ORDER': (r) => res.status === 200,
     //'Post Content-Type header': (r) => res.headers['Content-Type'] === 'application/json',
     'Post response is success ': (r) => res.status === 200 && res.json().success === true,
   });
 }
 
-const GETApi = () => {
+const RECORDApi = () => {
 
-  const location = 'location=' + GETKeys().location;
-  const date  = 'data=' + GETKeys().date;
-  const url = 'http://34.123.52.100:30100/api/report?';
+  const location = 'location=' + RECORDKeys().location;
+  const date  = 'data=' + RECORDKeys().date;
+  const url = 'http://34.80.73.51:80/api/record?';
   const sent = url.concat(location, '&', date);
-  const res = http.get('http://34.123.52.100:30100/api/record?location=l1&date=2023-01-01');
-  //const res = http.get(sent);
-
+  //const res = http.get('http://34.123.52.100:30100/api/record?location=l1&date=2023-01-01');//stable
+  const res = http.get('http://34.80.73.51:80/api/record?location=l1&date=2023-01-01');//old
+  //const res = http.get(sent);//random
+  //console.log(res);
   check(res, {
-    'GET status is 200 -2': (r) => res.status === 200,
+    'GET status is 200 -RECORD': (r) => res.status === 200,
   });
 }
 
+const REPORTApi = () => {
+  const data = JSON.stringify({
+    "location": "l1",
+    "timestamp": "2021-03-01T00:00:00.000Z",
+    "data": {
+      "a": 18,
+      "b": 8,
+      "c": 6,
+      "d": 28
+    }
+  });
+
+  const location  = 'location='.concat('l1');
+  const date = 'date='.concat('2021-03-01T00:00:00.000Z');
+  const url = 'http://34.80.73.51:8100/api/report?';
+  const sent = url.concat(location, '&', date);
+  const res = http.get(sent);
+  console.log(res.json().material);
+  console.log(res.json().a);
+  console.log(res.json().b);
+  console.log(res.json().c);
+  console.log(res.json().d);
+  check(res,{
+    'GET status is 200 -REPORT': (r) => res.status === 200,
+    'date & location is correct': (r) => res.json().location === 'l1',
+  })
+}
 export default function () {
-  POSTApi();
-  //GETApi();
+  //ORDERApi();
+  RECORDApi();
+  //REPORTApi();
 }
